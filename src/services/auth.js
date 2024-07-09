383a3c75a551ad497fa9b7d365807c7b05465735
Ok1za <oksanaprescott@gmail.com>
2024-07-09 18:50:46 +0300

    done

--- src/services/auth.js
+++ src/services/auth.js
@@ -11,6 +11,7 @@ import { FIFTEEN_MINUTES, TEMPLATES_DIR, THIRTY_DAYS } from "../constants/index.
 import { env } from '../utils/env.js';
 import { sendEmail } from "../utils/sendMail.js";
 import { SMTP } from "../constants/constants.js";
+import { getFullNameFromGoogleTokenPayload, validateCode } from "../utils/googleOAuth2.js";
 
 const createSession = () => {
     const accessToken = randomBytes(30).toString('base64');
@@ -157,4 +158,30 @@ export const resetPassword = async ({ token, password }) => {
         { _id: user._id },
         { password: encryptedPassword },
     );
+};
+
+export const loginOrSignupWithGoogle = async (code) => {
+    const loginTicket = await validateCode(code);
+
+    const payload = loginTicket.getPayload();
+
+    if (!payload) throw createHttpError(401);
+
+    let user = await UsersCollection.findOne({ email: payload.email });
+
+    if (!user) {
+        const password = await bcrypt.hash(randomBytes(10), 10);
+        user = await UsersCollection.create({
+            email: payload.email,
+            name: getFullNameFromGoogleTokenPayload(payload),
+            password,
+        });
+    }
+
+    const newSession = createSession();
+
+    return await SessionsCollection.create({
+        userId: user._id,
+        ...newSession,
+    });
 };
